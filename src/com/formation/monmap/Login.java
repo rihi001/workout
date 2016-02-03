1a52f6cc14d91d9732029ad9379ecf9100b5f923
RIHI <RIHI@192.168.1.2>
2016-02-03 23:48:07 +0000

    ok1

--- src/com/formation/monmap/Login.java
+++ src/com/formation/monmap/Login.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Login extends Activity {
-
+//ayoub
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
